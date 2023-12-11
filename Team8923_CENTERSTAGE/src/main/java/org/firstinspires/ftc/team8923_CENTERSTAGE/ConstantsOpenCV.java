1fbbd1382dc8e939c2cc6d267863c488603e7c53
Sanj Saini <>
2023-12-10 16:36:11 -0800

    Changed blue min saturation value to 60

--- Team8923_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team8923_CENTERSTAGE/ConstantsOpenCV.java
+++ Team8923_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team8923_CENTERSTAGE/ConstantsOpenCV.java
@@ -9,7 +9,7 @@ public class ConstantsOpenCV {
     public static final Scalar RED_COLOR_DETECT_MAX_HSV = new Scalar(9, 255, 255);
     public static final Scalar RED_COLOR_DETECT_MIN_HSV = new Scalar(0, 60, 50);
     public static final Scalar BLUE_COLOR_DETECT_MAX_HSV = new Scalar(120, 255, 255);
-    public static final Scalar BLUE_COLOR_DETECT_MIN_HSV = new Scalar(108, 50, 50);
+    public static final Scalar BLUE_COLOR_DETECT_MIN_HSV = new Scalar(108, 60, 50);
     public static final Scalar borderColor = new Scalar(0, 255, 0);  // green
     public static final int CAMERA_IMAGE_WIDTH = 640;
     public static final int CAMERA_IMAGE_HEIGHT = 480;
