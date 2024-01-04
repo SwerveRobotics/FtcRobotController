3423bbac0e2fbf19ed5819961f074943c96c2606
Reese G <rm9eor9e@gmail.com>
2024-01-03 18:22:04 -0800

    possibly a major telemetry funnies breakthrough?!?!?

---
+++ Team6220_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team6220_CENTERSTAGE/teleOpClasses/telemetry_format_test.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.team6220_CENTERSTAGE.teleOpClasses;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+@TeleOp(name="telemetry format test", group="amogus1")
+public class telemetry_format_test extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.setDisplayFormat(Telemetry.DisplayFormat.MONOSPACE);
+
+        waitForStart();
+
+        telemetry.addLine("test words! These are some WORDS..?");
+        telemetry.addLine("0123456789012345678901234567890123456789");
+        telemetry.addLine("Who knows if THIS will align correctly?");
+        telemetry.addLine("0123456789012345678901234567890123456789");
+        telemetry.addLine("fancy char: ➤[x]➤[x]1➤[2]➤[3]");
+        telemetry.update();
+
+        while (opModeIsActive()) {
+        }
+    }
+}
