14e405399ee7a34fe713832f8eb2c2d58ad8fd32
Eric Goossen <fadago@outlook.com>
2024-01-04 20:23:28 -0800

    started to add a basic drive forward test function. Started to add auto arm position not-macro.

---
+++ Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/MotionTests.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.team417_CENTERSTAGE;
+/*
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.team417_CENTERSTAGE.baseprograms.BaseOpMode;
+import org.firstinspires.ftc.team417_CENTERSTAGE.baseprograms.PropDistanceFactory;
+import org.firstinspires.ftc.team417_CENTERSTAGE.roadrunner.MecanumDrive;
+
+@TeleOp(name = "Motion Tests")
+public class MotionTests extends BaseOpMode {
+
+    public void runOpMode() {
+        driveFourFeet();
+    }
+
+    private void setPower(double power) {
+        drive.leftFront.setPower(power);
+        drive.rightFront.setPower(power);
+        drive.leftBack.setPower(power);
+        drive.rightBack.setPower(power);
+    }
+
+    public void driveFourFeet() {
+        Pose2d startPose = new Pose2d(0, 0, 0);
+
+        TrajectoryActionBuilder builder
+                = this.drive.actionBuilder(startPose)
+                .splineToLinearHeading(new Pose2d(96, 0, 0), 0);
+        return new PropDistanceFactory.PoseAndAction(builder.build(), startPose);
+    }
+}*/
