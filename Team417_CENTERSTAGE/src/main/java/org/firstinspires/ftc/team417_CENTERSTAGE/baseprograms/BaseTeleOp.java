ff697d354584e99ee287affa18d7a86200bdc992
DESKTOP-RC1PREA\Testing <2001290@apps.nsd.org>
2024-01-05 19:14:57 -0800

    Made USE_APRIL_TAGS public static to change in April Tags

--- Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/baseprograms/BaseTeleOp.java
+++ Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/baseprograms/BaseTeleOp.java
@@ -21,7 +21,7 @@ public abstract class BaseTeleOp extends BaseOpMode {
 
     ElapsedTime time = new ElapsedTime();
 
-    private final boolean USE_APRIL_TAGS = false;
+    public static boolean USE_APRIL_TAGS = false;
 
     public AprilTagPoseEstimator myATPoseEstimator;
 
