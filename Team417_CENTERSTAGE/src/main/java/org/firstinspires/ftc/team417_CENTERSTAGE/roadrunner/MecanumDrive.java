b90bd473d1da5a0dd8e5699ade070173743d3204
DESKTOP-RC1PREA\Testing <2001290@apps.nsd.org>
2024-01-27 22:25:42 -0800

    Made MaxWheelVel larger

--- Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/roadrunner/MecanumDrive.java
+++ Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/roadrunner/MecanumDrive.java
@@ -133,13 +133,13 @@ public final class MecanumDrive {
             }
 
             // path profile parameters (in inches)
-            maxWheelVel = 20; // was 50 before
+            maxWheelVel = 50;
             minProfileAccel = -30;
             maxProfileAccel = 50;
 
             // turn profile parameters (in radians)
-            maxAngVel = Math.PI * (2.0/5.0); // shared with path // was pi before
-            maxAngAccel = Math.PI * (2.0/5.0); // was pi before
+            maxAngVel = Math.PI; // shared with path
+            maxAngAccel = Math.PI;
 
             axialVelGain = 0.0;
             lateralVelGain = 0.0;
