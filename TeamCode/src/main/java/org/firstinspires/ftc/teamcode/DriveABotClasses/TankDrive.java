4f7ba84581492ceef1b2af50882e03331973080c
Gavin <gmacgaming40@gmail.com>
2024-03-12 20:57:00 -0700

    renamed the opmode from the automated one (i didnt forget to stage this change shhhhhhh tell no one)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveABotClasses/TankDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveABotClasses/TankDrive.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp(name = "TankDrive (Blocks to Java)")
+@TeleOp(name = "TankDrive")
 public class TankDrive extends LinearOpMode {
 
   private DcMotor MotorR;
