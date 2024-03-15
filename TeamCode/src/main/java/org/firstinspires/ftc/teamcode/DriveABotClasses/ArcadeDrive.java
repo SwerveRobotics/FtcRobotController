b0ebe3ce9f8145ad2ea43011cabdc2261a122926
Gavin <gmacgaming40@gmail.com>
2024-03-14 20:36:13 -0700

    Prettified seconds display for the drive-A-bots

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveABotClasses/ArcadeDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveABotClasses/ArcadeDrive.java
@@ -4,12 +4,17 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import java.util.Timer;
 
 @TeleOp(name = "ArcadeDrive")
 public class ArcadeDrive extends LinearOpMode {
 
   private DcMotor MotorR;
   private DcMotor MotorL;
+  ElapsedTime timer = new ElapsedTime();
+  int timeCapSeconds = 120;
 
   /**
    * This function is executed when this Op Mode is selected from the Driver Station.
@@ -19,20 +24,27 @@ public class ArcadeDrive extends LinearOpMode {
     MotorR = hardwareMap.get(DcMotor.class, "mot_right");
     MotorL = hardwareMap.get(DcMotor.class, "mot_left");
 
-
     MotorR.setDirection(DcMotorSimple.Direction.REVERSE);
     waitForStart();
+    timer.reset();
 
     if (opModeIsActive()) {
 
-      while (opModeIsActive()) {
-
-        MotorL.setPower(-(gamepad1.left_stick_y + gamepad1.right_stick_x));
-        MotorR.setPower(-(gamepad1.left_stick_y - gamepad1.right_stick_x));
-        telemetry.addData("Left Pow", MotorL.getPower());
-        telemetry.addData("Right Pow", MotorR.getPower());
+      while (opModeIsActive() && timer.seconds() <= timeCapSeconds) {
+        MotorL.setPower(-gamepad1.left_stick_y + gamepad1.right_stick_x);
+        MotorR.setPower(-gamepad1.left_stick_y - gamepad1.right_stick_x);
+        telemetry.addData("Time Remaining", convertSecondsToString(timeCapSeconds - (int)timer.seconds()));
         telemetry.update();
       }
     }
+  }
+
+  private String convertSecondsToString(int seconds) {
+    String str = "";
+    if (seconds >= 60) {
+      str += seconds / 60 + "m ";
+    }
+    str += seconds % 60 + "s";
+    return str;
   }
 }
