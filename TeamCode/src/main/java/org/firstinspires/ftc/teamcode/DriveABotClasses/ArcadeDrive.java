d0b69a3f65d2d7a76ec31e709d877728b329d7dc
Gavin <gmacgaming40@gmail.com>
2024-03-14 18:47:16 -0700

    this should maybe work for arcade drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveABotClasses/ArcadeDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveABotClasses/ArcadeDrive.java
@@ -5,8 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp(name = "TankDrive")
-public class TankDrive extends LinearOpMode {
+@TeleOp(name = "ArcadeDrive")
+public class ArcadeDrive extends LinearOpMode {
 
   private DcMotor MotorR;
   private DcMotor MotorL;
@@ -19,21 +19,16 @@ public class TankDrive extends LinearOpMode {
     MotorR = hardwareMap.get(DcMotor.class, "mot_right");
     MotorL = hardwareMap.get(DcMotor.class, "mot_left");
 
-    // Reverse one of the drive motors.
-    // You will have to determine which motor to reverse for your robot.
-    // In this example, the right motor was reversed so that positive
-    // applied power makes it move the robot in the forward direction.
+
     MotorR.setDirection(DcMotorSimple.Direction.REVERSE);
     waitForStart();
+
     if (opModeIsActive()) {
-      // Put run blocks here.
+
       while (opModeIsActive()) {
-        // Put loop blocks here.
-        // The Y axis of a joystick ranges from -1 in its topmost position
-        // to +1 in its bottommost position. We negate this value so that
-        // the topmost position corresponds to maximum forward power.
-        MotorL.setPower(-(gamepad1.left_stick_y / 1));
-        MotorR.setPower(-(gamepad1.right_stick_y / 1));
+
+        MotorL.setPower(-(gamepad1.left_stick_y + gamepad1.right_stick_x));
+        MotorR.setPower(-(gamepad1.left_stick_y - gamepad1.right_stick_x));
         telemetry.addData("Left Pow", MotorL.getPower());
         telemetry.addData("Right Pow", MotorR.getPower());
         telemetry.update();
