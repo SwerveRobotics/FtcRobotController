2ea84389374196d6de4e1b0eeb79054688824f22
DESKTOP-RC1PREA\Testing <2001290@apps.nsd.org>
2024-06-05 21:08:08 -0700

    Change sample program in Main.java

--- SimpleConsole/src/main/java/com/example/simpleconsole/Main.java
+++ SimpleConsole/src/main/java/com/example/simpleconsole/Main.java
@@ -1,10 +1,8 @@
 package com.example.simpleconsole;
 
-import com.example.simpleconsole.examples.session2.Loops;
-
 public class Main {
     // This is the main coding space for new members!
     public static void main(String[] args) {
-        Loops.main(args);
+        System.out.println("Hello World");
     }
 }
