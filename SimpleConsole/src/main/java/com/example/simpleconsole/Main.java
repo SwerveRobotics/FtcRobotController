a7d8d55b9699c376831602ca1d5c33c07f300e57
DESKTOP-RC1PREA\Testing <2001290@apps.nsd.org>
2024-06-06 17:43:21 -0700

    Changed Main to be "Hello world!"

--- SimpleConsole/src/main/java/com/example/simpleconsole/Main.java
+++ SimpleConsole/src/main/java/com/example/simpleconsole/Main.java
@@ -3,6 +3,6 @@ package com.example.simpleconsole;
 public class Main {
     // This is the main coding space for new members!
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello world!");
     }
 }
