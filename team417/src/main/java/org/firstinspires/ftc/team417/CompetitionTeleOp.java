949b266cf0d2fe9a597a99f7bc4b780387ae78d7
Team <417skid@swerverobotics.club>
2024-09-22 15:41:27 -0700

    Added TeleOp Code

--- team417/src/main/java/org/firstinspires/ftc/team417/CompetitionTeleOp.java
+++ team417/src/main/java/org/firstinspires/ftc/team417/CompetitionTeleOp.java
@@ -7,6 +7,8 @@ import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.PoseVelocity2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.team417.roadrunner.Drawing;
 import org.firstinspires.ftc.team417.roadrunner.MecanumDrive;
@@ -15,9 +17,12 @@ import org.firstinspires.ftc.team417.roadrunner.MecanumDrive;
  * This class exposes the competition version of TeleOp. As a general rule, add code to the
  * BaseOpMode class rather than here so that it can be shared between both TeleOp and Autonomous.
  */
-@TeleOp(name="TeleOp", group="Competition")
+@TeleOp(name = "TeleOp", group = "Competition")
 public class CompetitionTeleOp extends BaseOpMode {
     private double speedMultiplier = 1;
+    DcMotor armMotor;
+    DcMotor bootWheelServo;
+    Servo rotationServo;
 
     @Override
     public void runOpMode() {
@@ -25,6 +30,9 @@ public class CompetitionTeleOp extends BaseOpMode {
 
         Pose2d beginPose = new Pose2d(0, 0, 0);
         MecanumDrive drive = new MecanumDrive(kinematicType, hardwareMap, telemetry, gamepad1, beginPose);
+        armMotor = hardwareMap.get(DcMotor.class, "ArmMotor");
+        bootWheelServo = hardwareMap.get(DcMotor.class, "BootWheelServo");
+        rotationServo = hardwareMap.get(Servo.class, "RotationServo");
 
         // Wait for Start to be pressed on the Driver Hub!
         waitForStart();
@@ -50,6 +58,10 @@ public class CompetitionTeleOp extends BaseOpMode {
             // Update the current pose:
             drive.updatePoseEstimate();
 
+            controlArm();
+            controlRotation();
+            controlBootWheel();
+
             telemetry.addLine("Running TeleOp!");
             telemetry.addData("Kinematic Type", kinematicType);
             telemetry.addData("Speed Multiplier", speedMultiplier);
@@ -66,5 +78,29 @@ public class CompetitionTeleOp extends BaseOpMode {
             Drawing.drawRobot(packet.fieldOverlay(), drive.pose);
             MecanumDrive.sendTelemetryPacket(packet);
         }
+    }
+
+    public void controlArm() {
+        armMotor.getCurrentPosition();
+        armMotor.setPower(gamepad2.right_stick_y);
+    }
+
+    public final double leftRotationServo = 0;
+    public final double middleRotationServo = 0.5;
+    public final double rightRotationServo = 1;
+
+    public void controlRotation() {
+        if (gamepad2.dpad_up) {
+            rotationServo.setPosition(middleRotationServo);
+        } else if (gamepad2.dpad_left) {
+            rotationServo.setPosition(leftRotationServo);
+        } else if (gamepad2.dpad_right) {
+            rotationServo.setPosition(rightRotationServo);
+        }
+    }
+
+    public void controlBootWheel() {
+        double servoInput = gamepad2.right_trigger - gamepad2.left_trigger;
+        bootWheelServo.setPower(servoInput);
     }
 }
