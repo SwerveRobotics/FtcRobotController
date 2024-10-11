e92eecf20e1767d8c11465ed6a5b85abc5042600
Gavin <gthemaker032@gmail.com>
2024-10-10 20:50:28 -0700

    Added initial color sensor code. It may not be much, and it may not work (it doesnt), but it's something.

--- Team6220/src/main/java/org/firstinspires/ftc/team6220/Constants.java
+++ Team6220/src/main/java/org/firstinspires/ftc/team6220/Constants.java
@@ -8,4 +8,7 @@ public abstract class Constants {
     public static final int SLIDES_MOTOR_GROUND_POSITION = 0;
     public static final int SLIDES_MOTOR_POSITION_ONE = 20;
     public static final int SLIDES_MOTOR_POSITION_TWO = 40;
+    public static final float[] RED_COLOR_SENSOR_RANGES = {0.025f, 1f};
+    public static final float[] BLUE_COLOR_SENSOR_RANGES = {0.025f, 1f};
+    public static final float COLOR_SENSOR_GAIN = 3.8f;
 }
