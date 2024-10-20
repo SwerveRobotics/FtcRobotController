abaa3a5427cd30a8ea5f0596c79c760476ef375b
Ronith-Neelam1429 <1108552@apps.nsd.org>
2024-10-20 14:09:40 -0700

    Spline basket

--- team417/src/main/java/org/firstinspires/ftc/team417/CompetitionAuto.java
+++ team417/src/main/java/org/firstinspires/ftc/team417/CompetitionAuto.java
@@ -14,9 +14,11 @@ import org.firstinspires.ftc.team417.roadrunner.MecanumDrive;
  */
 @Autonomous(name = "Auto", group = "Competition", preselectTeleOp = "CompetitionTeleOp")
 public class CompetitionAuto extends BaseOpMode {
+    // Original 17.50
     final double ROBOT_LENGTH = 17.75;
-    // RC bot is 16.50
+    // Orginal 16.50
     final double ROBOT_WIDTH = 18.50;
+
     @Override
     public void runOpMode() {
 
@@ -31,19 +33,48 @@ public class CompetitionAuto extends BaseOpMode {
         // as soon as the Start button is pressed!
 
         // Position relative to basket, far is away from the basket vice versa
-        boolean isFar;
 
 
+        trajectoryAction = drive.actionBuilder(beginPose)
+                .setTangent(Math.toRadians(-45))
+                .splineToLinearHeading(new Pose2d(50, 50, Math.toRadians(45)), Math.toRadians(-45))
+                .build();
 
-        /*
-        if(true) {
-            trajectoryAction = drive.actionBuilder(new Pose2d(-24.00, 60.00, Math.PI * 1.5))
-                    .splineToLinearHeading();
-        } else{
-            trajectoryAction = drive.actionBuilder(new Pose2d(24.00, 60.00, Math.PI * 1.5))
-                .splineToLinearHeading();
+        Canvas previewCanvas = new Canvas();
+        trajectoryAction.preview(previewCanvas);
+
+
+        // Show the preview on FTC Dashboard now.
+        TelemetryPacket packet = MecanumDrive.getTelemetryPacket();
+        packet.fieldOverlay().getOperations().addAll(previewCanvas.getOperations());
+        MecanumDrive.sendTelemetryPacket(packet);
+
+
+        // Wait for Start to be pressed on the Driver Hub!
+        waitForStart();
+
+
+        boolean more = true;
+        while (opModeIsActive() && more) {
+            telemetry.addLine("Running Auto!");
+            telemetry.addData("Kinematic Type", kinematicType);
+            telemetry.update();
+
+
+            // 'packet' is the object used to send data to FTC Dashboard:
+            packet = MecanumDrive.getTelemetryPacket();
+
+
+            // Draw the preview and then run the next step of the trajectory on top:
+            packet.fieldOverlay().getOperations().addAll(previewCanvas.getOperations());
+            more = trajectoryAction.run(packet);
+
+
+            // Only send the packet if there's more to do in order to keep the very last
+            // drawing up on the field once the robot is done:
+            if (more)
+                MecanumDrive.sendTelemetryPacket(packet);
         }
-        */
-
     }
 }
+
