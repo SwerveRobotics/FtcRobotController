cdb654d234121156139a2f2bc7cb9967cc67f21b
anya-codes <anya.rajyaguru@gmail.com>
2024-10-20 13:31:30 -0700

    Created 3 new classes for ParkAutoLeft, ParkAutoMiddle, and ParkAutoRight for 3 different auto parking paths.

---
+++ Team6220/src/main/java/org/firstinspires/ftc/team6220/ParkAutoMiddle.java
@@ -0,0 +1,65 @@
+package org.firstinspires.ftc.team6220;
+
+import com.acmerobotics.dashboard.canvas.Canvas;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.team6220.javatextmenu.MenuFinishedButton;
+import org.firstinspires.ftc.team6220.javatextmenu.MenuInput;
+import org.firstinspires.ftc.team6220.javatextmenu.TextMenu;
+import org.firstinspires.ftc.team6220.roadrunner.MecanumDrive;
+
+/**
+ * This class exposes the competition version of Autonomous. As a general rule, add code to the
+ * BaseOpMode class rather than here so that it can be shared between both TeleOp and Autonomous.
+ */
+@Autonomous(name="ParkAutoMiddle", group="Competition", preselectTeleOp="CompetitionTeleOp")
+public class ParkAutoMiddle extends BaseOpMode {
+
+    @Override
+    public void runOpMode() {
+
+        Pose2d middlePose = new Pose2d(0, 60, (3*Math.PI)/2);
+
+        MecanumDrive drive = new MecanumDrive(hardwareMap, telemetry, gamepad1, middlePose);
+
+        // Create left parking trajectory
+        Action  middleParkingTrajectory = drive.actionBuilder(middlePose)
+                .splineTo(new Vector2d(-60, 60), (Math.PI))
+                .build();
+        Action trajectoryAction = middleParkingTrajectory;
+
+        // Get a preview of the trajectory's path:
+        Canvas previewCanvas = new Canvas();
+        trajectoryAction.preview(previewCanvas);
+
+        // Show the preview on FTC Dashboard now.
+        TelemetryPacket packet = MecanumDrive.getTelemetryPacket();
+        packet.fieldOverlay().getOperations().addAll(previewCanvas.getOperations());
+        MecanumDrive.sendTelemetryPacket(packet);
+
+        // Wait for Start to be pressed on the Driver Hub!
+        waitForStart();
+
+        boolean more = true;
+        while (opModeIsActive() && more) {
+            telemetry.addLine("Running Auto!");
+
+            // 'packet' is the object used to send data to FTC Dashboard:
+            packet = MecanumDrive.getTelemetryPacket();
+
+            // Draw the preview and then run the next step of the trajectory on top:
+            packet.fieldOverlay().getOperations().addAll(previewCanvas.getOperations());
+            more = trajectoryAction.run(packet);
+
+            // Only send the packet if there's more to do in order to keep the very last
+            // drawing up on the field once the robot is done:
+            if (more)
+                MecanumDrive.sendTelemetryPacket(packet);
+            telemetry.update();
+        }
+    }
+}
