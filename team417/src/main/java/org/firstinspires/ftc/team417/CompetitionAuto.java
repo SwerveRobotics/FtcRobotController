31a6b2dfaed63ffc11435deec0995a65658af1ac
Ronith-Neelam1429 <1108552@apps.nsd.org>
2024-10-20 13:30:49 -0700

    New branch for auto

--- team417/src/main/java/org/firstinspires/ftc/team417/CompetitionAuto.java
+++ team417/src/main/java/org/firstinspires/ftc/team417/CompetitionAuto.java
@@ -14,9 +14,14 @@ import org.firstinspires.ftc.team417.roadrunner.MecanumDrive;
  */
 @Autonomous(name = "Auto", group = "Competition", preselectTeleOp = "CompetitionTeleOp")
 public class CompetitionAuto extends BaseOpMode {
+    final double ROBOT_LENGTH = 17.75;
+    // RC bot is 16.50
+    final double ROBOT_WIDTH = 18.50;
     @Override
     public void runOpMode() {
-        Pose2d beginPose = new Pose2d(0, 0, 0);
+
+        // BeginPose is the 2nd tile away from the basket, facing the basket, lined up with the tile boundary
+        Pose2d beginPose = new Pose2d((ROBOT_LENGTH / 2) + 24, 72 - (ROBOT_WIDTH / 2), 0);
         MecanumDrive drive = new MecanumDrive(kinematicType, hardwareMap, telemetry, gamepad1, beginPose);
 
         Action trajectoryAction;
@@ -24,57 +29,21 @@ public class CompetitionAuto extends BaseOpMode {
         // Build the trajectory *before* the start button is pressed because Road Runner
         // can take multiple seconds for this operation. We wouldn't want to have to wait
         // as soon as the Start button is pressed!
-        if (false) {
+
+        // Position relative to basket, far is away from the basket vice versa
+        boolean isFar;
+
+
+
+        /*
+        if(true) {
             trajectoryAction = drive.actionBuilder(new Pose2d(-24.00, 60.00, Math.PI * 1.5))
-                    .splineToLinearHeading(new Pose2d(-48.00, 60.00, Math.PI * 1.5), Math.PI)
-                    .splineToLinearHeading(new Pose2d(-60.00, 48.00, Math.PI * 1.6), Math.PI / 2)
-                    .splineToLinearHeading(new Pose2d(-60.00, 60.00, Math.PI * 1.5), Math.PI / 2)
-                    .splineToLinearHeading(new Pose2d(-60.00, 48.00, Math.PI * 1.5), Math.PI / 2)
-                    .splineToLinearHeading(new Pose2d(-60.00, 60.00, Math.PI * 1.5), Math.PI / 2)
-                    .splineToLinearHeading(new Pose2d(-60.00, 48.00, Math.PI * 1.4), Math.PI / 2)
-                    .splineToLinearHeading(new Pose2d(-60.00, 60.00, Math.PI * 1.5), Math.PI / 2)
-                    .build();
-        } else {
+                    .splineToLinearHeading();
+        } else{
             trajectoryAction = drive.actionBuilder(new Pose2d(24.00, 60.00, Math.PI * 1.5))
-                    .splineToLinearHeading(new Pose2d(48.00, 60.00, Math.PI * 1.5), 0)
-                    .splineToLinearHeading(new Pose2d(60.00, 48.00, Math.PI * 1.4), Math.PI / 2)
-                    .splineToLinearHeading(new Pose2d(60.00, 60.00, Math.PI * 1.25), Math.PI / 2)
-                    .splineToLinearHeading(new Pose2d(60.00, 48.00, Math.PI * 1.5), Math.PI / 2)
-                    .splineToLinearHeading(new Pose2d(60.00, 60.00, Math.PI * 1.25), Math.PI / 2)
-                    .splineToLinearHeading(new Pose2d(60.00, 48.00, Math.PI * 1.6), Math.PI / 2)
-                    .splineToLinearHeading(new Pose2d(60.00, 60.00, Math.PI * 1.25), Math.PI / 2)
-                    .build();
+                .splineToLinearHeading();
         }
+        */
 
-        // Get a preview of the trajectory's path:
-        Canvas previewCanvas = new Canvas();
-        trajectoryAction.preview(previewCanvas);
-
-        // Show the preview on FTC Dashboard now.
-        TelemetryPacket packet = MecanumDrive.getTelemetryPacket();
-        packet.fieldOverlay().getOperations().addAll(previewCanvas.getOperations());
-        MecanumDrive.sendTelemetryPacket(packet);
-
-        // Wait for Start to be pressed on the Driver Hub!
-        waitForStart();
-
-        boolean more = true;
-        while (opModeIsActive() && more) {
-            telemetry.addLine("Running Auto!");
-            telemetry.addData("Kinematic Type", kinematicType);
-            telemetry.update();
-
-            // 'packet' is the object used to send data to FTC Dashboard:
-            packet = MecanumDrive.getTelemetryPacket();
-
-            // Draw the preview and then run the next step of the trajectory on top:
-            packet.fieldOverlay().getOperations().addAll(previewCanvas.getOperations());
-            more = trajectoryAction.run(packet);
-
-            // Only send the packet if there's more to do in order to keep the very last
-            // drawing up on the field once the robot is done:
-            if (more)
-                MecanumDrive.sendTelemetryPacket(packet);
-        }
     }
 }
