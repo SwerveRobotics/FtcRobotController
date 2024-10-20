00c8805015c184b3ee226f37f8297485aef297f7
bharv <bharvi.doshi08@gmail.com>
2024-10-20 14:15:46 -0700

    Scoring paths auto

---
+++ Team6220/src/main/java/org/firstinspires/ftc/team6220/MiddleScoringAuto.java
@@ -0,0 +1,92 @@
+package org.firstinspires.ftc.team6220;
+
+import com.acmerobotics.dashboard.canvas.Canvas;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.team6220.javatextmenu.MenuInput;
+import org.firstinspires.ftc.team6220.javatextmenu.TextMenu;
+import org.firstinspires.ftc.team6220.roadrunner.MecanumDrive;
+
+/**
+ * This class exposes the competition version of Autonomous. As a general rule, add code to the
+ * BaseOpMode class rather than here so that it can be shared between both TeleOp and Autonomous.
+ */
+@Autonomous(name="Auto", group="Competition", preselectTeleOp="CompetitionTeleOp")
+public class MiddleScoringAuto extends BaseOpMode {
+
+
+    @Override
+    public void runOpMode() {
+
+        Pose2d middlePose = new Pose2d(0, 60, (3*Math.PI)/2);
+
+
+        MecanumDrive drive = new MecanumDrive(hardwareMap, telemetry, gamepad1, middlePose);
+
+        Action middleScoringTrajectory     = drive.actionBuilder(middlePose)
+                .splineTo(new Vector2d(48, 36), (3*Math.PI)/2)
+                .endTrajectory()
+                .splineTo(new Vector2d(48, 50), (5*Math.PI)/4)
+                .build();
+
+        Action trajectoryAction = middleScoringTrajectory;
+
+        // Get a preview of the trajectory's path:
+        Canvas previewCanvas = new Canvas();
+        trajectoryAction.preview(previewCanvas);
+
+        // Show the preview on FTC Dashboard now.
+        TelemetryPacket packet = MecanumDrive.getTelemetryPacket();
+        packet.fieldOverlay().getOperations().addAll(previewCanvas.getOperations());
+        MecanumDrive.sendTelemetryPacket(packet);
+
+        // Wait for Start to be pressed on the Driver Hub!
+        waitForStart();
+
+        boolean more = true;
+        while (opModeIsActive() && more) {
+            telemetry.addLine("Running Auto!");
+
+            // 'packet' is the object used to send data to FTC Dashboard:
+            packet = MecanumDrive.getTelemetryPacket();
+
+            // Draw the preview and then run the next step of the trajectory on top:
+            packet.fieldOverlay().getOperations().addAll(previewCanvas.getOperations());
+            more = trajectoryAction.run(packet);
+
+            // Only send the packet if there's more to do in order to keep the very last
+            // drawing up on the field once the robot is done:
+            if (more)
+                MecanumDrive.sendTelemetryPacket(packet);
+            telemetry.update();
+        }
+    }
+
+    private void textMenuUpdateUntilComplete(TextMenu textMenu, MenuInput input) {
+        while (!textMenu.isCompleted() && !isStopRequested()) {
+            for (String line : textMenu.toListOfStrings()) {
+                telemetry.addLine(line);
+            }
+            telemetry.update();
+
+            input.update(
+                    gamepad1.left_stick_x, gamepad1.left_stick_y,
+                    gamepad1.dpad_left, gamepad1.dpad_right,
+                    gamepad1.dpad_down, gamepad1.dpad_up,
+                    gamepad1.a
+            );
+            textMenu.updateWithInput(input);
+            sleep(17);
+        }
+    }
+
+    private <E extends Enum> void runIfNotNull (Class<E> enumClass, Runnable runnable) {
+        if (enumClass != null) {
+
+        }
+    }
+}
