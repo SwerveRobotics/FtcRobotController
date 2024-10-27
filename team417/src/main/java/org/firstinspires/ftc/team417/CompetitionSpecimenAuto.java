6cc3f59343e3718b196e659d23f9b4b17bca779d
Team <417skid@swerverobotics.club>
2024-10-27 15:39:29 -0700

    Auto Changes (Complete)

--- team417/src/main/java/org/firstinspires/ftc/team417/CompetitionSpecimenAuto.java
+++ team417/src/main/java/org/firstinspires/ftc/team417/CompetitionSpecimenAuto.java
@@ -1,9 +1,10 @@
 package org.firstinspires.ftc.team417;
 
+import com.acmerobotics.dashboard.canvas.Canvas;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.robot.Robot;
 import com.wilyworks.common.WilyWorks;
 
 import org.firstinspires.ftc.team417.roadrunner.MecanumDrive;
@@ -19,9 +20,46 @@ public class CompetitionSpecimenAuto extends BaseOpMode {
         initializeHardware();
         Action trajectoryAction = drive.actionBuilder(beginPose)
                 .setTangent(Math.toRadians(-90))
-                .splineToLinearHeading(new Pose2d((ROBOT_LENGTH / -2), Y_SCORE_POSE, Math.toRadians(45)), Math.toRadians(-45))
+                .splineToLinearHeading(new Pose2d((ROBOT_LENGTH / -2), Y_SCORE_POSE, Math.toRadians(-90)), Math.toRadians(-90))
+                .afterDisp(0, new MoveArm(ARM_SCORE_SPECIMEN, WRIST_FOLDED_IN))
                 .build();
 
+
+        Canvas previewCanvas = new Canvas();
+        trajectoryAction.preview(previewCanvas);
+
+
+        // Show the preview on FTC Dashboard now.
+        TelemetryPacket packet = MecanumDrive.getTelemetryPacket();
+        packet.fieldOverlay().getOperations().addAll(previewCanvas.getOperations());
+        MecanumDrive.sendTelemetryPacket(packet);
+
+
+        // Wait for Start to be pressed on the Driver Hub!
+        waitForStart();
+
+
+        boolean more = true;
+        while (opModeIsActive() && more) {
+            telemetry.addLine("Running Auto!");
+            telemetry.addData("Kinematic Type", kinematicType);
+
+
+            // 'packet' is the object used to send data to FTC Dashboard:
+            packet = MecanumDrive.getTelemetryPacket();
+
+
+            // Draw the preview and then run the next step of the trajectory on top:
+            packet.fieldOverlay().getOperations().addAll(previewCanvas.getOperations());
+            more = trajectoryAction.run(packet);
+
+
+            // Only send the packet if there's more to do in order to keep the very last
+            // drawing up on the field once the robot is done:
+            if (more)
+                MecanumDrive.sendTelemetryPacket(packet);
+            telemetry.update();
+        }
     }
 
 
