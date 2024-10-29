3fad834c99b478fdd81d054169563fcfc6008f35
Hankang Zhou <136044263+nobody-particular@users.noreply.github.com>
2024-10-28 19:41:42 -0700

    Added a skeleton class

---
+++ team417/src/main/java/org/firstinspires/ftc/team417/distance/DistanceLocalizer.java
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.team417.distance;
+
+public class DistanceLocalizer {
+    public UltrasonicDistanceSensor leftDistance, rightDistance;
+    public DistanceSensorInfo leftInfo, rightInfo;
+}
