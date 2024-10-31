0071216ea124a18f1779ec730a6207de1c132a49
Gavin <gthemaker032@gmail.com>
2024-10-30 20:30:39 -0700

    Updated auto path to park at submersible and score more consistently

--- Team6220/src/main/java/org/firstinspires/ftc/team6220/ScoringAutoRightPushbot.java
+++ Team6220/src/main/java/org/firstinspires/ftc/team6220/ScoringAutoRightPushbot.java
@@ -3,9 +3,6 @@ package org.firstinspires.ftc.team6220;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-import org.apache.commons.math3.analysis.function.Constant;
-import org.firstinspires.ftc.robotcore.external.Const;
-
 @Autonomous(name = "ScoringAutoRightPushbot", group = "Competition", preselectTeleOp = "CompetitionTeleOp")
 public class ScoringAutoRightPushbot extends ScoringAutoMiddlePushbot{
 
