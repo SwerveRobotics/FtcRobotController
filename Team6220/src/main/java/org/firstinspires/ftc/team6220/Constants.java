6917d57eb176e2701b0d7467be78115d9fee7473
Gavin <gthemaker032@gmail.com>
2024-10-30 19:55:21 -0700

    edited constant for ground position of arm

--- Team6220/src/main/java/org/firstinspires/ftc/team6220/Constants.java
+++ Team6220/src/main/java/org/firstinspires/ftc/team6220/Constants.java
@@ -9,7 +9,7 @@ public abstract class Constants {
     public static double ARM_ELBOW_SERVO_PRESET_POSITION_GROUND = 0.25;
     public static int ARM_BASE_MOTOR_POSITION_GROUND = -1680;
     public static int ARM_BASE_MOTOR_POSITION_FOLD = 0;
-    public static int ARM_BASE_MOTOR_POSITION_SUB = -1450;
+    public static int ARM_BASE_MOTOR_POSITION_SUB = -1500;
     public static int SLIDES_MOTOR_GROUND_POSITION = 0;
     public static int SLIDES_MOTOR_POSITION_ONE = 20;
     public static int SLIDES_MOTOR_POSITION_TWO = 40;
@@ -20,6 +20,6 @@ public abstract class Constants {
     public static final float COLOR_SENSOR_GAIN = 3.8f;
     //public static final Pose2d BASKET_SCORING_POSE = new Pose2d()
     public static final Pose2d MIDDLE_STARTING_POSE = new Pose2d(0, 60, (3*Math.PI)/2);
-    public static final Pose2d LEFT_STARTING_POSE = new Pose2d(-24, 60, (3*Math.PI)/2);
-    public static final Pose2d RIGHT_STARTING_POSE = new Pose2d(24, 60, (3*Math.PI)/2);
+    public static final Pose2d LEFT_STARTING_POSE = new Pose2d(24, 60, (3*Math.PI)/2);
+    public static final Pose2d RIGHT_STARTING_POSE = new Pose2d(-24, 60, (3*Math.PI)/2);
 }
