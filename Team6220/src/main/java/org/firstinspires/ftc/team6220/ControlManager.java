9181c16555da1396a8350fff5039493cc27bae26
bharv <bharvi.doshi08@gmail.com>
2024-11-17 13:37:16 -0800

    Added commments to ControlManager to make it easier to read.

--- Team6220/src/main/java/org/firstinspires/ftc/team6220/ControlManager.java
+++ Team6220/src/main/java/org/firstinspires/ftc/team6220/ControlManager.java
@@ -20,28 +20,42 @@ public class ControlManager {
     }
 
     public void update() {
+        // Raise arm elbow to go over submersible bar
         if(gamepad2.a && !gamepad2.b) {
             armElbowServoPosition =  DRIFTConstants.ARM_ELBOW_SERVO_POSITION_OVER_BAR;
         }
+
+        // Lower arm elbow to pick up samples
         if(gamepad2.b && !gamepad2.a) {
             armElbowServoPosition = DRIFTConstants.ARM_ELBOW_SERVO_POSITION_GROUND;
         }
-        if(gamepad2.x && !gamepad2.y) {
-            armBaseMotorPosition = DRIFTConstants.ARM_BASE_MOTOR_POSITION_GROUND;
-        }
+
+        // Lowering arm all the way to ground (NOT BEING USED)
+//        if(gamepad2.x && !gamepad2.y) {
+//            armBaseMotorPosition = DRIFTConstants.ARM_BASE_MOTOR_POSITION_GROUND;
+//        }
+
+        // Raises the arm up to its initial position (against the hubs)
         if (gamepad2.y && !gamepad2.x) {
             armBaseMotorPosition = DRIFTConstants.ARM_BASE_MOTOR_POSITION_INIT;
         }
+
+        // Powers the intake
         intakeServoPower = -gamepad2.left_stick_y;
+
+        // Lowers the arm to go over the submersible
+        if (gamepad2.right_bumper && !gamepad2.left_bumper) {
+            armBaseMotorPosition = DRIFTConstants.ARM_BASE_MOTOR_POSITION_OVER_BAR;
+        }
+
+        // Lowers the slides to their initial position
         if (gamepad2.dpad_down && !gamepad2.dpad_up) {
             slidesMotorPosition = DRIFTConstants.SLIDES_MOTOR_GROUND_POSITION;
         }
-        if (gamepad2.dpad_up && !gamepad2.dpad_down) {
-            slidesMotorPosition = DRIFTConstants.SLIDES_MOTOR_POSITION_ONE;
-        }
-        if (gamepad2.right_bumper && !gamepad2.left_bumper) {
-            armBaseMotorPosition = DRIFTConstants.ARM_BASE_MOTOR_POSITION_OVER_BAR;
 
+        // Raises the slides to reach the high basket
+        if (gamepad2.dpad_up && !gamepad2.dpad_down) {
+            slidesMotorPosition = DRIFTConstants.SLIDES_MOTOR_POSITION_TWO;
         }
     }
 
