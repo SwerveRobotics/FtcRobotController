2f277d5c62e01689901d150471661e5a67c07d76
Team <417skid@swerverobotics.club>
2025-01-11 18:22:20 -0800

    Color sensors should work

--- team417/src/main/java/org/firstinspires/ftc/team417/BaseOpModeSlowBot.java
+++ team417/src/main/java/org/firstinspires/ftc/team417/BaseOpModeSlowBot.java
@@ -48,10 +48,10 @@ abstract public class BaseOpModeSlowBot extends LinearOpMode {
     public static double LIFT_SCORE_LOW_BASKET = 750;
     public static double LIFT_SCORE_HIGH_SPECIMEN = 875;
     public static double LIFT_GET_SPECIMEN = 650;
-    public static double LIFT_COLLECT = 30.0;
+    public static double LIFT_COLLECT = 75.0;
     public static double LIFT_MIN = 0.0;
     public static double LIFT_CLEAR_BARRIER = 10.0;
-    public static double LIFT_HOME_POSITION = 0;
+    public static double LIFT_HOME_POSITION = 20;
     public static double LIFT_NO_SLIDE_ZONE_MIN = 100;
     public static double LIFT_NO_SLIDE_ZONE_MAX = 1000;
 
