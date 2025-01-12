22212c9ec668ae9591f7e606cd262a8ff0ce1cc1
Team <417skid@swerverobotics.club>
2025-01-11 17:13:01 -0800

    new LIFT values

--- team417/src/main/java/org/firstinspires/ftc/team417/BaseOpModeSlowBot.java
+++ team417/src/main/java/org/firstinspires/ftc/team417/BaseOpModeSlowBot.java
@@ -48,12 +48,12 @@ abstract public class BaseOpModeSlowBot extends LinearOpMode {
     public static double LIFT_SCORE_LOW_BASKET = 750;
     public static double LIFT_SCORE_HIGH_SPECIMEN = 875;
     public static double LIFT_GET_SPECIMEN = 650;
-    public static double LIFT_COLLECT = 75.0;
+    public static double LIFT_COLLECT = 30.0;
     public static double LIFT_MIN = 0.0;
     public static double LIFT_CLEAR_BARRIER = 10.0;
     public static double LIFT_HOME_POSITION = 0;
     public static double LIFT_NO_SLIDE_ZONE_MIN = 100;
-    public static double LIFT_NO_SLIDE_ZONE_MAX = 800;
+    public static double LIFT_NO_SLIDE_ZONE_MAX = 1000;
 
     public static double f_coefficient = 0.5;
 
