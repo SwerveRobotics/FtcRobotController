61fa671d56f8f4970304bd1acc6f420184b53f67
Andrew <andrewgo@hotmail.com>
2025-03-18 10:33:28 -0700

    Somewhat better tuned arm specs.

--- TeamMentor/src/main/java/org/firstinspires/ftc/teamMentor/Arm.java
+++ TeamMentor/src/main/java/org/firstinspires/ftc/teamMentor/Arm.java
@@ -30,19 +30,19 @@ class ArmSpecs {
     static final String CALIBRATION_FILE = "/sdcard/arm_calibration.json";
 
     static final double MAX_SERVO_SPEED = Math.toRadians(60) / 0.17; // Marcel's specs: 60 degrees in 0.17 seconds
-    static final Point TURRET_OFFSET = new Point(-3, 0); // Relative to robot center
-    static final Point SHOULDER_OFFSET = new Point(0, 1.25); // Offset of shoulder relative to turret base
-    static final double SEGMENT_WIDTH = 2.5; // Width of the arm segments
+    static final Point TURRET_OFFSET = new Point(-4, 1); // Relative to robot center
+    static final Point SHOULDER_OFFSET = new Point(0, 0.5); // Offset of shoulder relative to turret base
+    static final double SEGMENT_WIDTH = 2.75; // Width of the arm segments
     static final double CLAW_Y_OFFSET = -SEGMENT_WIDTH; // Offset of the claw in y, relative to the shoulder's center
 
-    static final double SHOULDER_HEIGHT = 4; // Height of the shoulder joint relative to floor
-    static final double SEGMENT_OVERHANG = SEGMENT_WIDTH / 2; // Overhang of the arm segment past the joint
-    static final double SEGMENT_LENGTH = 14.0; // Length of an arm segment from joint to joint
+    static final double SHOULDER_HEIGHT = 8; // Height of the shoulder joint relative to floor
+    static final double SEGMENT_OVERHANG = 2; // Overhang of the arm segment past the joint
+    static final double SEGMENT_LENGTH = 13.0; // Length of an arm segment from joint to joint
     static final double LAST_SEGMENT_LENGTH = 3.0; // Length of the last arm segment
     static final double CLAW_OFFSET = 2; // Offset from the end of the last segment to the claw
-    static final double Y_BOUNDS = 4; // Bounds of the arm in the positive Y direction
+    static final double Y_BOUNDS = 4; // Positive Y value that bounds the arm, on either side of the turret base
     static final double X_CLAW_BOUNDS = 2; // Additional X bounds to account for the claw
-    static final double SUBMERSIBLE_HEIGHT = 4; // Distance from claw tip to floor when in submersible
+    static final double SUBMERSIBLE_HEIGHT = 4; // Target distance from claw tip to floor when in submersible
 }
 
 /**
