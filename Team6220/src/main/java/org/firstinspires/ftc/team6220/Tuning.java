cd7d21ae90cde902cd2e0bd6d182a41e28434258
Andrew <93226767+madacker@users.noreply.github.com>
2026-09-17 17:31:51 -0700

    Biobuzz staging (#73)
    Add Pedro Pathing quickstart.

---
+++ Team6220/src/main/java/org/firstinspires/ftc/team6220/Tuning.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.team6220;
+
+public class Tuning {
+    // Tuners go here
+}
---
+++ team417/src/main/java/org/firstinspires/ftc/team417/Tuning.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.team417;
+
+public class Tuning {
+    // Tuners go here
+}
