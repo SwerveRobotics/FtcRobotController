915dc2b744a60520587eafb6d44a6329291b242d
bharv <bharvi.doshi08@gmail.com>
2026-09-20 16:47:22 -0700

    Basic auto structure

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionAuto.java
@@ -1,9 +1,22 @@
 package org.firstinspires.ftc.teamcode;
+import com.pedropathing.follower.Follower;
+import org.firstinspires.ftc.teamcode.pedro.Constants;
+import com.pedropathing.api.PoseFactory;
+import com.pedropathing.math.Pose;
+import static com.pedropathing.api.Paths.*;
 
+import com.pedropathing.paths.Path;
 public class CompetitionAuto extends BaseOpMode{
+    private Follower follower;
+    private final PoseFactory p = PoseFactory.degrees();
+    private final Pose startPose = p.of(24, 24, 0);//placeholder values, use visualizer
+    private final Pose park = p.of(48, 48, 90);
+
 
     @Override
     public void runOpMode() throws InterruptedException {
+        follower = Constants.create(hardwareMap);
+        follower.setPose(startPose);
 
     }
 }
