952f10b31ad3e04628fe789f6a76505eee67957f
arisha_codes <arisha.rajyaguru@gmail.com>
2026-09-20 16:19:29 -0700

    Started code for CompetitionTeleOp, CompetitionAuto and BaseOpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseOpMode.java
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+abstract public class BaseOpMode extends LinearOpMode {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionAuto.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "417 Auto")
+public class CompetitionAuto extends BaseOpMode{
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
