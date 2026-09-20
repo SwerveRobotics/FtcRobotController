e9b424c510bd5a12ac58ac7d728bf99d9c2f1a27
Andrew <93226767+madacker@users.noreply.github.com>
2026-09-19 17:12:47 -0700

    Biobuzz staging (#74)
    Switch to TeamCode and enable Sloth.

--- Team6220/src/main/java/org/firstinspires/ftc/team6220/Tuning.java
+++
@@ -1,5 +0,0 @@
-package org.firstinspires.ftc.team6220;
-
-public class Tuning {
-    // Tuners go here
-}
--- team417/src/main/java/org/firstinspires/ftc/team417/Tuning.java
+++
@@ -1,5 +0,0 @@
-package org.firstinspires.ftc.team417;
-
-public class Tuning {
-    // Tuners go here
-}
