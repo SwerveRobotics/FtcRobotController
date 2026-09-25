44fdffaca6276ebc405ff6473eeb23082d9d3066
Kashvi Garg <kashvi.gk@gmail.com>
2026-09-24 18:55:07 -0700

    Added telemetry to test forward translational test. Started TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CompetitionTeleOp.java
@@ -1,11 +1,39 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.pedropathing.follower.Follower;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp(name = "417 Teleop")
+import org.firstinspires.ftc.teamcode.BaseOpMode;
+import org.firstinspires.ftc.teamcode.Constants;
+
+@TeleOp(name="417 Teleop")
 public class CompetitionTeleOp extends BaseOpMode {
+    private Follower follower;
+
     @Override
     public void runOpMode() throws InterruptedException {
+        follower = Constants.create(hardwareMap);
+        waitForStart();
+        while (opModeIsActive()) {
+            double forward = -gamepad1.left_stick_y;
+            double lateral = gamepad1.left_stick_x;
+            double turn = gamepad1.right_stick_x;
+
+
+            //"The driver wants to move this way."
+            //Pedro then figures out what the drivetrain motors need to do.
+            follower.manual(forward, lateral, turn);
+            follower.update();
+            //add telemetry on driver station X, Y, Heading follower.pose.getheading .getY  .getX
+        }
+
+        while (opModeIsActive()) {
+            //add intake controls here
+            //add follower update
+        }
+        //slowmodeeeee
 
     }
+
+
 }
